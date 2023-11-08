b3737041361e066e116ae54b132ecd6edf774a94
vishalgddam873 <vishal18@navgurukul.org>
2023-11-08 12:00:53 +0530

    Code Updated

--- TA1/main.py
+++ TA1/main.py
@@ -4,29 +4,28 @@ from email.mime.multipart import MIMEMultipart
 # Import tkinter library
 
 # Pass Tk as parameter
+
+
 class EmailSenderApp():
     def __init__(self):
         super().__init__()
         # Set the title Email Sender
 
-
         # Create and arrange GUI elements
-        
 
         # Create a Label with the name Sender's Email.
-        
 
         # Create a Label with the name Sender's Password.
-        
 
-        
+        # Create a Label with the name Message.
+
     def send_single_email(self):
         sender_email = input("Enter Sender Email\n")
         sender_password = input("Enter Sender Password\n")
         recipient_email = input("Enter Recipient Email\n")
         subject = input("Enter Email Subject\n")
         message_body = input("Enter Message\n")
-        
+
         try:
             smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
             smtp_server.starttls()
@@ -38,8 +37,9 @@ class EmailSenderApp():
             message["To"] = recipient_email
             message["Subject"] = subject
             message.attach(MIMEText(message_body, "plain"))
-      
-            smtp_server.sendmail(sender_email, recipient_email, message.as_string())
+
+            smtp_server.sendmail(
+                sender_email, recipient_email, message.as_string())
             smtp_server.quit()
             print("Email Sent", "Email sent successfully!")
 
@@ -51,7 +51,7 @@ def main():
     app = EmailSenderApp()
     # Instead of send_single_email call mainloop() function
     app.send_single_email()
-    
+
 
 if __name__ == "__main__":
     main()
